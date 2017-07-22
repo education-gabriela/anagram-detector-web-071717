ea32e2898d403edcfc2993ccae801033f01f6f73
gabidavila <gabidavila@gmail.com>
2017-07-22 10:47:36 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    ordered_word = self.class.order_word(self.word)
+
+    words_array.select do |word|
+      ordered_word == self.class.order_word(word)
+    end
+
+  end
+
+  def self.order_word(word)
+    word.split("").sort
+  end
+end
